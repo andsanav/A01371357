1c7dea4dc4e8e3ed1a6afd0cb2ccd8452f44f105
Andrea <Andrea@tec-cem-064-001.cem.itesm.mx>
2017-10-02 10:38:23 -0500

    Ejercicio Búsqueda secuencial

---
+++ Práctica 6/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+	
+	public int LinearSearch(int[] data, int target){
+		for(int i = 0;i < data.length;i++){
+			if(data[i] == target){
+					return 1;}
+			if(data[i] > target){
+				return -1;}}
+			return -1;
+	}
+				
+
+	public static int main(String[] args) {
+		
+		
+		
+				
+			
+		}
+
+	}
+
+}
